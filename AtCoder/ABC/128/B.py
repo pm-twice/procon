1501c5da8cdbdcd47ac8777d0d8b549dd8d61716
pm-twice <47174886+pm-twice@users.noreply.github.com>
2019-05-27 12:48:44 +0900

    [fix] AtCoder ABC 128: Problem B
    よりシンプルな解法に変更

--- AtCoder/ABC/128/B.py
+++ AtCoder/ABC/128/B.py
@@ -1,21 +1,17 @@
 #-*- coding:utf-8 -*-
 
+# Pythonのソートが安定であることを利用する
+# まず、得点で降順でソートして、市名で昇順ソートする
+
 N = int(input())
-ls = {}
+ls = [None]*N
 for i in range(N):
     s, p = input().split()
-    if s not in ls:
-        ls[s] = [(i+1, int(p))]
-    else:
-        ls[s].append((i+1, int(p)))
+    ls[i] = (i+1, s, int(p))
+ls.sort(key=(lambda x: -x[2]))  # pで降順ソート
+ls.sort(key=(lambda x: x[1]))   # sで昇順ソート (安定ソートなので、同じsについてpが降順となる)
 
-r = [(k,v) for k, v in ls.items()]
-r.sort(key=(lambda x: x[0]))
-
-for l in r:
-    vals = l[1]
-    vals.sort(key=(lambda x: x[1]), reverse=True)
-    for v in vals:
-        print(v[0])
+for i, s, p in ls:
+    print(i)
 
 
