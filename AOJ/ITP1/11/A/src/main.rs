eb8822a93d51d001b0e10da4f071bbbbba3cdacc
pm-twice <47174886+pm-twice@users.noreply.github.com>
2020-02-14 12:26:17 +0900

    [rewite] AOJ ITP1_11
    構造体を書きなおし、流れが分かりやすいように整理した

--- AOJ/ITP1/11/A/src/main.rs
+++ AOJ/ITP1/11/A/src/main.rs
@@ -2,75 +2,97 @@
 
 use std::io;
 
-/// 0: 上面
-/// 1: 手前
-/// 2: 右
-/// 3: 左
-/// 4: 奥
-/// 5: 下面
+/// u:上, f:前,l:左, b:奥, r:右, d:下
 #[derive(Debug)]
 struct Dice {
-    surface: Vec<u32>,
+    u: u32, f: u32, r: u32, l: u32, b: u32, d: u32, 
 }
 
+#[allow(dead_code)]
 impl Dice {
-    fn new(surface: &Vec<u32>) -> Dice {
-        assert_eq!(surface.len(), 6);
+    fn new(u: u32, f: u32, r: u32, l: u32, b: u32, d: u32) -> Dice {
+        Dice {u, f, r, l, b, d}
+    }
+
+    fn from_vec(vec: &Vec<u32>) -> Dice {
+        assert_eq!(vec.len(), 6);
         Dice {
-            surface: surface.clone(),
+            u: vec[0], f: vec[1], r: vec[2], l: vec[3], b: vec[4], d: vec[5],
+        }
+    }
+
+    fn from_dice(dice: &Dice) -> Dice {
+        Dice {u: dice.u, f: dice.f, r: dice.r, l: dice.l, b: dice.b, d: dice.d, }
+    }
+
+    fn move_s(&mut self){
+        let tmp = self.u;
+        self.u = self.b;
+        self.b = self.d;
+        self.d = self.f;
+        self.f = tmp;
+    }
+
+    fn move_n(&mut self) {
+        let tmp = self.u;
+        self.u = self.f;
+        self.f = self.d;
+        self.d = self.b;
+        self.b = tmp;
+    }
+
+    fn move_e(&mut self) {
+        let tmp = self.u;
+        self.u = self.l;
+        self.l = self.d;
+        self.d = self.r;
+        self.r = tmp;
+    }
+
+    fn move_w(&mut self) {
+        let tmp = self.u;
+        self.u = self.r;
+        self.r = self.d;
+        self.d = self.l;
+        self.l = tmp;
+    }
+
+    fn rotate_r(&mut self){
+        let tmp = self.f;
+        self.f = self.l;
+        self.l = self.b;
+        self.b = self.r;
+        self.r = tmp;
+    }
+
+    fn rotate_l(&mut self){
+        let tmp = self.f;
+        self.f = self.r;
+        self.r = self.b;
+        self.b = self.l;
+        self.l = tmp;
+    }
+
+    fn operate(&mut self, op: char){
+        match op {
+            'S' => self.move_s(),
+            'N' => self.move_n(),
+            'W' => self.move_w(),
+            'E' => self.move_e(),
+            'R' => self.rotate_r(),
+            'L' => self.rotate_l(),
+            _ => (),
+        };
+    }
+
+    fn operate_string(&mut self, ops: &String) {
+        for c in ops.chars() {
+            self.operate(c);
         }
     }
 
     fn top(&self) -> u32 {
-        self.surface[0]
-    }
-
-    fn move_s(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[4];   // 奥→上面
-        self.surface[4] = self.surface[5];   // 下面→奥
-        self.surface[5] = self.surface[1];   // 手前→下面
-        self.surface[1] = tmp;   // 上面→手前
-    }
-
-    fn move_n(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[1];   // 手前→上
-        self.surface[1] = self.surface[5];   // 下面→手前
-        self.surface[5] = self.surface[4];   // 奥→下面
-        self.surface[4] = tmp;   // 上面→奥
-    }
-
-    fn move_e(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[3];   // 左→上面
-        self.surface[3] = self.surface[5];   // 下面→左
-        self.surface[5] = self.surface[2];   // 右→下面
-        self.surface[2] = tmp;  // 上面→右
-    }
-
-    fn move_w(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[2];   // 右→上面
-        self.surface[2] = self.surface[5];   // 下面→右
-        self.surface[5] = self.surface[3];   // 左→下面
-        self.surface[3] = tmp;  // 上面→左
-    }
-
-    fn move_op(&mut self, op: char){
-        match op {
-            'N' => { self.move_n(); },
-            'E' => { self.move_e(); },
-            'W' => { self.move_w(); },
-            'S' => { self.move_s(); },
-            _ => { ; },
-        }
-    }
-
-    fn move_ops(&mut self, ops: &String){
-        for op in ops.chars() {
-            self.move_op(op);
-        }
+        self.u
     }
 }
 
@@ -79,13 +101,14 @@ fn main() {
     let mut buf = String::new();
     sin.read_line(&mut buf).ok();
     let v = buf.split_whitespace()
-        .map(|v| v.parse::<u32>().unwrap())
+        .map(|v| v.parse::<u32>().unwrap() )
         .collect::<Vec<u32>>();
-    let mut d = Dice::new(&v);
+
+    let mut dice = Dice::from_vec(&v);
 
     let mut ops = String::new();
     sin.read_line(&mut ops).ok();
-    d.move_ops(&ops);
-
-    println!("{}", d.top());
+    
+    dice.operate_string(&ops);
+    println!("{}", dice.top());
 }
--- AOJ/ITP1/11/B/src/main.rs
+++ AOJ/ITP1/11/B/src/main.rs
@@ -1,126 +1,145 @@
 #![allow(non_snake_case)]
 
 use std::io;
+use std::io::prelude::*;
 
-/// 0: 上面
-/// 1: 手前
-/// 2: 右
-/// 3: 左
-/// 4: 奥
-/// 5: 下面
+/// u:上, f:前,l:左, b:奥, r:右, d:下
 #[derive(Debug)]
 struct Dice {
-    surface: Vec<u32>,
+    u: u32, f: u32, r: u32, l: u32, b: u32, d: u32, 
 }
 
 #[allow(dead_code)]
 impl Dice {
-    fn new(surface: &Vec<u32>) -> Dice {
-        assert_eq!(surface.len(), 6);
+    fn new(u: u32, f: u32, r: u32, l: u32, b: u32, d: u32) -> Dice {
+        Dice {u, f, r, l, b, d}
+    }
+
+    fn from_vec(vec: &Vec<u32>) -> Dice {
+        assert_eq!(vec.len(), 6);
         Dice {
-            surface: surface.clone(),
+            u: vec[0], f: vec[1], r: vec[2], l: vec[3], b: vec[4], d: vec[5],
+        }
+    }
+
+    fn from_dice(dice: &Dice) -> Dice {
+        Dice {u: dice.u, f: dice.f, r: dice.r, l: dice.l, b: dice.b, d: dice.d, }
+    }
+
+    fn move_s(&mut self){
+        let tmp = self.u;
+        self.u = self.b;
+        self.b = self.d;
+        self.d = self.f;
+        self.f = tmp;
+    }
+
+    fn move_n(&mut self) {
+        let tmp = self.u;
+        self.u = self.f;
+        self.f = self.d;
+        self.d = self.b;
+        self.b = tmp;
+    }
+
+    fn move_e(&mut self) {
+        let tmp = self.u;
+        self.u = self.l;
+        self.l = self.d;
+        self.d = self.r;
+        self.r = tmp;
+    }
+
+    fn move_w(&mut self) {
+        let tmp = self.u;
+        self.u = self.r;
+        self.r = self.d;
+        self.d = self.l;
+        self.l = tmp;
+    }
+
+    fn rotate_r(&mut self){
+        let tmp = self.f;
+        self.f = self.l;
+        self.l = self.b;
+        self.b = self.r;
+        self.r = tmp;
+    }
+
+    fn rotate_l(&mut self){
+        let tmp = self.f;
+        self.f = self.r;
+        self.r = self.b;
+        self.b = self.l;
+        self.l = tmp;
+    }
+
+    fn operate(&mut self, op: char){
+        match op {
+            'S' => self.move_s(),
+            'N' => self.move_n(),
+            'W' => self.move_w(),
+            'E' => self.move_e(),
+            'R' => self.rotate_r(),
+            'L' => self.rotate_l(),
+            _ => (),
+        };
+    }
+
+    fn operate_string(&mut self, ops: &String) {
+        for c in ops.chars() {
+            self.operate(c);
         }
     }
 
     fn top(&self) -> u32 {
-        self.surface[0]
+        self.u
     }
 
-    fn move_s(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[4];   // 奥→上面
-        self.surface[4] = self.surface[5];   // 下面→奥
-        self.surface[5] = self.surface[1];   // 手前→下面
-        self.surface[1] = tmp;   // 上面→手前
-    }
+    fn question_right(&self, top: u32, front: u32) -> Option<u32> {
+        let mut v = (0..6).map(|_| Dice::from_dice(&self)).collect::<Vec<Dice>>();
 
-    fn move_n(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[1];   // 手前→上
-        self.surface[1] = self.surface[5];   // 下面→手前
-        self.surface[5] = self.surface[4];   // 奥→下面
-        self.surface[4] = tmp;   // 上面→奥
-    }
+        // 異なる上面6パターンを得る
+        v[1].move_e();
+        v[2].move_w();
+        v[3].move_n();
+        v[4].move_s();
+        v[5].move_s(); v[5].move_s();
 
-    fn move_e(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[3];   // 左→上面
-        self.surface[3] = self.surface[5];   // 下面→左
-        self.surface[5] = self.surface[2];   // 右→下面
-        self.surface[2] = tmp;  // 上面→右
-    }
-
-    fn move_w(&mut self){
-        let tmp = self.surface[0];
-        self.surface[0] = self.surface[2];   // 右→上面
-        self.surface[2] = self.surface[5];   // 下面→右
-        self.surface[5] = self.surface[3];   // 左→下面
-        self.surface[3] = tmp;  // 上面→左
-    }
-
-    fn move_op(&mut self, op: char){
-        match op {
-            'N' => { self.move_n(); },
-            'E' => { self.move_e(); },
-            'W' => { self.move_w(); },
-            'S' => { self.move_s(); },
-            _ => { ; },
+        for d in v.iter_mut() {
+            // 横回転4パターンで全てを探索
+            for _ in 0..4 {
+                if d.u == top && d.f == front {
+                    return Some(d.r);
+                }
+                d.rotate_r();
+            }
         }
-    }
 
-    fn move_ops(&mut self, ops: &String){
-        for op in ops.chars() {
-            self.move_op(op);
-        }
-    }
-
-    /// 上面と手前の面から、その時の右の面を返す
-    fn question_right(&self, top: u32, front: u32) -> u32 {
-        let mut d = Dice::new(&self.surface);
-        // とりあえず上面をtopにする
-        match d.surface.iter().position(|&i| {i==top}) {
-            Some(0) => { ; },
-            Some(1) => { d.move_n(); },
-            Some(2) => { d.move_w(); },
-            Some(3) => { d.move_e(); },
-            Some(4) => { d.move_s(); },
-            Some(5) => { d.move_s(); d.move_s(); },
-            _ => { panic!(); },
-        };
-
-        // frontに応じてrightがどこにあるか探す
-        let res = match d.surface.iter().position(|&i| {i==front}) {
-            Some(0) => { panic!("top & front: top error"); },
-            Some(1) => { d.surface[2] },    // frontが手前→rightは右(2)
-            Some(2) => { d.surface[4] },    // frontが右→rightは奥(4)
-            Some(3) => { d.surface[1] },    // frontが左→rightは手前(1)
-            Some(4) => { d.surface[3] },    // frontが奥→rightは左(3)
-            Some(5) => { panic!("top & front: bottom error") },    // frontが下面→存在しない
-            _ => { panic!(); },
-        };
-
-        res
+        None
     }
 }
 
 fn main() {
-    let sin = io::stdin();
-    let mut buf = String::new();
-    sin.read_line(&mut buf).ok();
-    let v = buf.split_whitespace()
-        .map(|v| v.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-    let d = Dice::new(&v);
 
-    let mut buf = String::new();
-    sin.read_line(&mut buf).ok();
-    let n = buf.trim().parse::<u32>().unwrap();
-    for _ in 0..n {
-        let mut buf = String::new();
-        sin.read_line(&mut buf).ok();
-        let mut ws = buf.split_whitespace();
+    let sin = io::stdin();
+    let mut l_iter = sin.lock().lines();
+
+    let v = l_iter.next().unwrap().unwrap().split_whitespace()
+        .map(|v| v.parse::<u32>().unwrap() )
+        .collect::<Vec<u32>>();
+
+    let dice = Dice::from_vec(&v);
+
+    let _n = l_iter.next().unwrap().unwrap().trim().parse::<usize>().unwrap();
+
+    for line in l_iter {
+        let l = line.unwrap();
+        let mut ws = l.split_whitespace();
         let top = ws.next().unwrap().parse::<u32>().unwrap();
         let front = ws.next().unwrap().parse::<u32>().unwrap();
-        println!("{}", d.question_right(top, front));
+        if let Some(x) = dice.question_right(top, front) {
+            println!("{}", x);
+        }
     }
 }
