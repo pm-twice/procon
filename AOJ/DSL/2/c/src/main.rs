2abe65a4a0bb9eb92259d0abdd0441c9a4989404
pm-twice <47174886+pm-twice@users.noreply.github.com>
2020-05-06 17:25:00 +0900

    [fix] AOJ DSL 2C with my snippet kdtree
    スニペットで実装したkd木を用いた形に実装を変更

--- AOJ/DSL/2/c/src/main.rs
+++ AOJ/DSL/2/c/src/main.rs
@@ -21,84 +21,90 @@ impl<R: Read> Scanner<R> {
     }
 }
 
-// ここでは2次元のkd木を作る
-struct Kdtree {
-    root: usize,
-    points: Vec<(i64, i64, usize)>, // x, y, idx
-    rights: Vec<Option<usize>>,
-    lefts: Vec<Option<usize>>,
+pub struct Kdtree2d {
+    points: Vec<(i64, i64, usize)>,
+    nodes: Vec<Node>,
 }
-
-impl Kdtree {
-    fn new(points: &Vec<(i64, i64)>) -> Kdtree {
+struct Node {
+    location: usize,
+    right: Option<usize>,
+    left: Option<usize>,
+}
+impl Kdtree2d {
+    pub fn new(points: &Vec<(i64, i64)>) -> Kdtree2d {
         let n = points.len();
-
-        let mut kd = Kdtree {
-            root: 0,
+        let mut kt = Kdtree2d {
             points: (0..n).map(|i| (points[i].0, points[i].1, i)).collect(),
-            rights: vec![None; n],
-            lefts: vec![None; n],
+            nodes: Vec::with_capacity(n),
         };
-
-        let r = kd.make_2dtree(0, n, 0);
-        kd.root = r.unwrap();
-
-        kd
+        kt.make_2dtree(0, n, 0);
+        kt
     }
-
-    /// O(n logn)のソートを複数回用いて、深さごとにx,yを対象にソートする
-    /// 中央値のindexを返す
-    fn make_2dtree(&mut self, left: usize, right: usize, depth: u64) -> Option<usize> {
+    fn make_2dtree(&mut self, left: usize, right: usize, depth: usize) -> Option<usize> {
         if !(left < right) {
             return None;
         }
-
         if depth % 2 == 0 {
             self.points[left..right].sort_by_key(|v| v.0);
         } else {
             self.points[left..right].sort_by_key(|v| v.1);
         }
-
         let mid: usize = (left + right) / 2;
-
-        self.lefts[mid] = self.make_2dtree(left, mid, depth+1);
-        self.rights[mid] = self.make_2dtree(mid+1, right, depth+1);
-
-        Some(mid)
+        let node = Node {
+            location: mid,
+            left: None,
+            right: None,
+        };
+        let idx = self.nodes.len();
+        self.nodes.push(node);
+        self.nodes[idx].left = self.make_2dtree(left, mid, depth + 1);
+        self.nodes[idx].right = self.make_2dtree(mid + 1, right, depth + 1);
+        Some(idx)
     }
-
-    fn find(&self, sx: i64, tx: i64, sy: i64, ty: i64) -> Vec<usize> {
+    pub fn find(&self, sx: i64, tx: i64, sy: i64, ty: i64) -> Vec<usize> {
         let mut results = vec![];
-        self._find(&mut results, self.root, sx, tx, sy, ty, 0);
+        self.find_internal(0, sx, tx, sy, ty, 0, &mut results);
         results.sort();
         results
     }
-
-    fn _find(&self, results: &mut Vec<usize>, idx: usize, sx: i64, tx: i64, sy: i64, ty: i64, depth: u64) {
+    fn find_internal(
+        &self,
+        node_idx: usize,
+        sx: i64,
+        tx: i64,
+        sy: i64,
+        ty: i64,
+        depth: usize,
+        results: &mut Vec<usize>,
+    ) {
+        let node = &self.nodes[node_idx];
+        let idx = node.location;
         let x = self.points[idx].0;
         let y = self.points[idx].1;
-
         if sx <= x && x <= tx && sy <= y && y <= ty {
             results.push(self.points[idx].2);
         }
-
         if depth % 2 == 0 {
-            if sx <= x && self.lefts[idx].is_some() {
-                let l = self.lefts[idx].unwrap();
-                self._find(results, l, sx, tx, sy, ty, depth+1);
-            } 
-            if x <= tx && self.rights[idx].is_some() {
-                let r = self.rights[idx].unwrap();
-                self._find(results, r, sx, tx, sy, ty, depth+1);
+            if let Some(l_idx) = node.left {
+                if x >= sx {
+                    self.find_internal(l_idx, sx, tx, sy, ty, depth + 1, results);
+                }
+            }
+            if let Some(r_idx) = node.right {
+                if x <= tx {
+                    self.find_internal(r_idx, sx, tx, sy, ty, depth + 1, results);
+                }
             }
         } else {
-            if sy <= y && self.lefts[idx].is_some() {
-                let l = self.lefts[idx].unwrap();
-                self._find(results, l, sx, tx, sy, ty, depth+1);
-            } 
-            if y <= ty && self.rights[idx].is_some() {
-                let r = self.rights[idx].unwrap();
-                self._find(results, r, sx, tx, sy, ty, depth+1);
+            if let Some(l_idx) = node.left {
+                if y >= sy {
+                    self.find_internal(l_idx, sx, tx, sy, ty, depth + 1, results);
+                }
+            }
+            if let Some(r_idx) = node.right {
+                if y <= ty {
+                    self.find_internal(r_idx, sx, tx, sy, ty, depth + 1, results);
+                }
             }
         }
     }
@@ -127,7 +133,7 @@ fn main() {
         (sx, tx, sy, ty)
     }).collect();
 
-    let kt = Kdtree::new(&points);
+    let kt = Kdtree2d::new(&points);
     let out = io::stdout();
     let mut out = BufWriter::new(out.lock());
     for (sx, tx, sy, ty) in squares {
